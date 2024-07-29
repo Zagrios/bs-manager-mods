aec08775d02edceeed2851c2c2bd3dd856cb50e6
Shidorien <gaetangerard@hotmail.fr>
2024-07-30 00:02:18 +0200

    feat: Add Discord OAuth provider for authentication

--- auth.ts
+++ auth.ts
@@ -1,7 +1,8 @@
 import NextAuth from "next-auth"
+import discord from "next-auth/providers/discord"
 import GitHub from "next-auth/providers/github"
 import google from "next-auth/providers/google"
  
 export const { handlers, auth } = NextAuth({
-  providers: [GitHub,google],
+  providers: [discord,GitHub,google],
 })
