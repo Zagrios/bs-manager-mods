2731baa21621e36f4ff56c11d5773628f4117430
MathieuG-P <40181755+Zagrios@users.noreply.github.com>
2024-07-30 20:24:41 +0200

    BaetMods2

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <h1 className="text-4xl font-bold text-center">Welcome to BeatMods!</h1>
+            <h1 className="text-4xl font-bold text-center">Welcome to BeatMods2!</h1>
         </main>
     );
 }
