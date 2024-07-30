e80cfc8b97eec73749a05ff5b165ce6ee14694cf
Shidorien <gaetangerard@hotmail.fr>
2024-07-30 18:21:03 +0200

    remove unussed var

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,13 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import { NavBar } from '@/components/nav-bar.component'
-import { Session } from 'next-auth'
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "BeatmodsV2",
   description: "Developed by the BSManager team",
 };
 
-export default function RootLayout({ session, children }: Readonly<{ session: Session, children: React.ReactNode }>) {
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
         <body>
