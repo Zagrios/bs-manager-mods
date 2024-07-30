a9e856f48e3f1a4f555c7f966aa9b2f34342ac70
MathieuG-P <40181755+Zagrios@users.noreply.github.com>
2024-07-30 20:18:45 +0200

    Setup prettier + add lint and format checks

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,21 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { NavBar } from '@/components/nav-bar.component'
+import { NavBar } from "@/components/nav-bar.component";
 
 export const metadata: Metadata = {
-  title: "BeatmodsV2",
-  description: "Developed by the BSManager team",
+    title: "BeatmodsV2",
+    description: "Developed by the BSManager team",
 };
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-  return (
-    <html lang="en">
-        <body>
-            <header className='w-full'>
-                <NavBar/>
-            </header>
-            {children}
-        </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body>
+                <header className="w-full">
+                    <NavBar />
+                </header>
+                {children}
+            </body>
+        </html>
+    );
 }
--- components/nav-bar.component.tsx
+++ components/nav-bar.component.tsx
@@ -1,9 +1,9 @@
-import { auth } from "@/auth"
+import { auth } from "@/auth";
 import Link from "next/link";
 
 export async function NavBar() {
     const session = await auth();
-    
+
     return (
         <div className="navbar bg-base-100">
             <div className="navbar-start">
@@ -20,5 +20,5 @@ export async function NavBar() {
                 )}
             </div>
         </div>
-    )
+    );
 }
