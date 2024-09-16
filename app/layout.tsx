bf554b74f3e220fc61a169ab7af32956cf1c6f40
Shidorien <gaetangerard@hotmail.fr>
2024-09-16 18:16:42 +0200

    init layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,20 @@
 import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import { NavBar } from "@/components/nav-bar.component";
 
 export const metadata: Metadata = {
-    title: "BeatmodsV2",
+    title: "Beatmods",
     description: "Developed by the BSManager team",
 };
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
     return (
         <html lang="en">
-            <body className="min-h-screen flex flex-col ">
+            <link rel="icon" href="/favicon.ico" sizes="any" />
+            <body className={inter.className + " min-h-screen flex flex-col "}>
                 <NavBar/>
                 {children}
             </body>
